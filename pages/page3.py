d5ecec7dafcf6cbb9a88ebaea2eeefbee8b67ee7
Vaughn2400 <kennethgolosinda24@gmail.com>
2023-08-10 14:38:20 +0800

    page4 added

--- pages/page3.py
+++ pages/page3.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
+from openpyxl import load_workbook
+
 
 def remove_sidebar():
 
@@ -15,17 +18,80 @@ def remove_sidebar():
         unsafe_allow_html=True,
     )
 
+def txt_to_list(txt_file_path):
+    """
+    This function turn text file into list
+    :param txt_file_path: This should be the text file that you want to turn in to a list
+    :return: list
+    """
+    with open(txt_file_path, 'r') as txt_file:
+        data_list = txt_file.read().splitlines()
+    return data_list
+
+
 if __name__ == '__main__':
     remove_sidebar()
-    st.title('Please upload an image')
+    st.title('Physiognomy Personality Test')
+    questions = txt_to_list('asset/list-of-questionsV2.txt')
+    # Questions and corresponding Likert scale options
+    likert_options = ['','Strongly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree']
+    # Initialize a dictionary to store user responses
+    responses = {}
+    # Loop through the questions and display them with a Likert scale widget
+    for idx, question in enumerate(questions):
+        if idx == 0 or idx == 1 or responses.get(f'Q{idx}'):
+            st.subheader(f'Q{idx + 1}: {question}')
+            key = f'question_{idx}'
+            #placeholder = st.empty()
+            st.markdown(
+                """ <style>
+                        div[role="radiogroup"] >  :first-child{
+                            display: none !important;
+                        }
+                    </style>
+                    """,
+                unsafe_allow_html=True
+            )
+            response = st.radio(' ', likert_options, key=key, horizontal=True, index=0)
+            responses[f'Q{idx + 1}'] = response
+            #placeholder.empty()
+
+    st.write('\n\n---\n\n')
+    #st.subheader('Survey Summary')
+
+    # Display the user responses in dataframe panda
+    response_table = [(f'Q{idx+1}', responses[question], likert_options.index(responses[question])) for idx, question in enumerate(responses)]
+    df_response_table = pd.DataFrame(response_table, columns=['Question', 'Response', 'Response Index'])
+
+
+    #st.dataframe(df_response_table)
+
     style = """<style>
     .row-widget.stButton {
     text-align: right;}
     </style>"""
+    st.markdown(style, unsafe_allow_html=True)
+    excel_file_path = "user_answers.xlsx"
+    sheet_name = "Sheet1"
+    existing_data = pd.read_excel(excel_file_path, sheet_name=sheet_name)
 
-    file = st.file_uploader("Pick a file.")
     if st.button("Next Page"):
-        switch_page("page4")
+        nan_check = (df_response_table['Response Index'] == 0).any()
+        if nan_check:
+            if st.button("Prev Page"):
+                switch_page("page2")
+            st.error("Please answer all the questions.")
+        else:
+            questions_no_list = df_response_table['Question']
+            response_index_list = df_response_table['Response Index']
+            for col_name, col_values in zip(questions_no_list, response_index_list):
+                existing_data.loc[0, col_name] = col_values
+                existing_data.to_excel(excel_file_path, sheet_name=sheet_name, index=False)
+            try:
+                st.success("Data successfully stored in the Excel file!")
+                switch_page("page4")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
     elif st.button("Prev Page"):
         switch_page("page2")
-
--- pages/page4.py
+++ pages/page4.py
@@ -1,9 +1,13 @@
 import streamlit as st
-import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
-from openpyxl import load_workbook
+import os
+import pandas as pd
+from PIL import Image
+import datetime
 
-
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
 def remove_sidebar():
 
     st.set_page_config(initial_sidebar_state="collapsed")
@@ -18,80 +22,35 @@ def remove_sidebar():
         unsafe_allow_html=True,
     )
 
-def txt_to_list(txt_file_path):
-    """
-    This function turn text file into list
-    :param txt_file_path: This should be the text file that you want to turn in to a list
-    :return: list
-    """
-    with open(txt_file_path, 'r') as txt_file:
-        data_list = txt_file.read().splitlines()
-    return data_list
-
-
 if __name__ == '__main__':
     remove_sidebar()
-    st.title('Physiognomy Personality Test')
-    questions = txt_to_list('asset/list-of-questionsV2.txt')
-    # Questions and corresponding Likert scale options
-    likert_options = ['','Strongly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree']
-    # Initialize a dictionary to store user responses
-    responses = {}
-    # Loop through the questions and display them with a Likert scale widget
-    for idx, question in enumerate(questions):
-        if idx == 0 or idx == 1 or responses.get(f'Q{idx}'):
-            st.subheader(f'Q{idx + 1}: {question}')
-            key = f'question_{idx}'
-            #placeholder = st.empty()
-            st.markdown(
-                """ <style>
-                        div[role="radiogroup"] >  :first-child{
-                            display: none !important;
-                        }
-                    </style>
-                    """,
-                unsafe_allow_html=True
-            )
-            response = st.radio(' ', likert_options, key=key, horizontal=True, index=0)
-            responses[f'Q{idx + 1}'] = response
-            #placeholder.empty()
-
-    st.write('\n\n---\n\n')
-    #st.subheader('Survey Summary')
-
-    # Display the user responses in dataframe panda
-    response_table = [(f'Q{idx+1}', responses[question], likert_options.index(responses[question])) for idx, question in enumerate(responses)]
-    df_response_table = pd.DataFrame(response_table, columns=['Question', 'Response', 'Response Index'])
-
-
-    #st.dataframe(df_response_table)
-
+    st.title('Please upload an image')
     style = """<style>
     .row-widget.stButton {
     text-align: right;}
     </style>"""
     st.markdown(style, unsafe_allow_html=True)
-    excel_file_path = "user_answers.xlsx"
-    sheet_name = "Sheet1"
-    existing_data = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+
+    image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+    if image_file is not None:
+        file_details = {"FileName": image_file.name, "FileType": image_file.type}
+        st.write(file_details)
+        img = load_image(image_file)
+        st.image(img)
+
+        df = pd.read_excel("user_answers.xlsx")
+        Name = df["First Name"][0] + " " + df["Last Name"][0]
+        current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        image_extension = image_file.name.split('.')[-1].lower()
+        image_file_name = str(Name+current_time)+"." + image_extension
+
+        #
+        with open(os.path.join("user_img", image_file_name), "wb") as f:
+            f.write(image_file.getbuffer())
+        st.success("Saved File")
 
     if st.button("Next Page"):
-        nan_check = (df_response_table['Response Index'] == 0).any()
-        if nan_check:
-            if st.button("Prev Page"):
-                switch_page("page3")
-            st.error("Please answer all the questions.")
-        else:
-            questions_no_list = df_response_table['Question']
-            response_index_list = df_response_table['Response Index']
-            for col_name, col_values in zip(questions_no_list, response_index_list):
-                existing_data.loc[0, col_name] = col_values
-                existing_data.to_excel(excel_file_path, sheet_name=sheet_name, index=False)
-            try:
-                st.success("Data successfully stored in the Excel file!")
-                switch_page("page5")
-            except Exception as e:
-                st.error(f"An error occurred: {e}")
-
+        switch_page("page5")
     elif st.button("Prev Page"):
         switch_page("page3")
+
